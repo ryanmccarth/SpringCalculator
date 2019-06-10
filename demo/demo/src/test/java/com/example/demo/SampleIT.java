a949be92add61a334ca0f694ed190e437365be3c
ryanmccarth <ryanfmccarthy30@gmail.com>
2019-06-10 14:34:25 -0400

    Integration Support to JaCoCo added
    Can now view reports of integration tests

---
+++ demo/demo/src/test/java/com/example/demo/SampleIT.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class SampleIT{
+
+  @Test
+  public void sampleTest(){
+    assertTrue(true);
+  }
+}
